83e4f07edf3c7b7eacac6f56223545cb21b7e20e
DocKurayami09 <doctorkuro00@gmail.com>
2025-10-13 12:56:55 +0200

    Premier skill de Naruto

---
+++ rasenga.py
@@ -0,0 +1,10 @@
+
+
+def Rasengan():
+    sss=input("are you an enemy ?")
+    if sss == "yes" :
+        return "Rasengaaaaannnnnn!!!!!!"
+    else:
+        return "Talk no Jutsu" 
+
+print(Rasengan())
